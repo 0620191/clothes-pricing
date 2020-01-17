61fbc9250667387168e766d9cc32a7414b7d75d6
Beverly Tan <60005730+0620191@users.noreply.github.com>
2020-01-17 14:18:41 +0000

    hollister data and processor

---
+++ hollister/prices.py
@@ -0,0 +1,7 @@
+#  Copyright Beverly Xin Chong Tan (c) 2020.
+
+from abercrombie.prices import get_prices_for as aber_get_prices_for
+
+
+def get_prices_for(file):
+    return aber_get_prices_for(file)
---
+++ hollister/urls.py
@@ -0,0 +1,3 @@
+jacketscoats = "./hollister/webpages/jacketscoats.html"
+tshirts = "./hollister/webpages/tshirts.html"
+jeans = "./hollister/webpages/jeans.html"
